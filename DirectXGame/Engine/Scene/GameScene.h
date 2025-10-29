bb56aa8d350b249fe32241ffa6aa0373e230b110
GSUser <GSUser@MSI>
2025-10-29 16:35:13 +0900

    クリックで回転できるように変更

--- DirectXGame/Engine/Scene/GameScene.h
+++ DirectXGame/Engine/Scene/GameScene.h
@@ -30,10 +30,9 @@ public:
 
 	void ChangePhase();
 
-
 	KamataEngine::Sprite* sprite_ = nullptr;
 
-	//uint32_t textureHandle_ = 0;	
+	// uint32_t textureHandle_ = 0;
 
 	MapChipField* mapChipField_ = nullptr;
 
@@ -76,17 +75,25 @@ private:
 
 	uint32_t textureHandleRight_ = KamataEngine::TextureManager::Load("./resources/arrow/right.png");
 	uint32_t textureHandleLeft_ = KamataEngine::TextureManager::Load("./resources/arrow/left.png");
+	// 追加: 回転ボタン用テクスチャ
+	uint32_t textureHandleRotate_ = KamataEngine::TextureManager::Load("./resources/arrow/rotate.png");
 
-	// スプライト（左右2つ）
-	KamataEngine::Sprite* spriteRight_ = nullptr; // 右移動トリガー（右側に表示するもの）
-	KamataEngine::Sprite* spriteLeft_ = nullptr;  // 左移動トリガー（左隣に表示するもの）
+	// スプライト（左右2つ + 上1つ）
+	KamataEngine::Sprite* spriteRight_ = nullptr;  // 右移動トリガー（右側に表示するもの）
+	KamataEngine::Sprite* spriteLeft_ = nullptr;   // 左移動トリガー（左隣に表示するもの）
+	KamataEngine::Sprite* spriteRotate_ = nullptr; // 回転トリガー（上に表示するもの）
 
 	// スプライトの左上位置（ピクセル）とサイズ（ピクセル）を保持（左上アンカー）
 	KamataEngine::Vector3 spriteRightPos_ = {300.0f, 300.0f, 0.0f};
 	KamataEngine::Vector3 spriteLeftPos_ = {80.0f, 300.0f, 0.0f}; // 初期は右の左隣に配置
+	// 追加: 回転ボタンの左上位置
+	KamataEngine::Vector3 spriteRotatePos_ = {300.0f, 140.0f, 0.0f};
+
 	KamataEngine::Vector3 spriteSize_ = {150.0f, 150.0f, 0.0f};
 
 	// 当たり判定と移動要求送信を行う汎用関数
 	// dx: -1 = 左移動要求, +1 = 右移動要求
 	bool ProcessSpriteClickMove(const KamataEngine::Vector3& spriteLeftTop, const KamataEngine::Vector3& spriteSize, int dx);
+	// 追加: 回転スプライトの判定（クリックでミノ回転を要求）
+	bool ProcessSpriteClickRotate(const KamataEngine::Vector3& spriteLeftTop, const KamataEngine::Vector3& spriteSize);
 };
